7dad66cfcde14f9cda7c8bd9b00443a47ba2fde0
Devendra <devendra_dewangan@outlook.com>
2026-04-15 19:10:26 +0530

    background fix and svg fix

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -4,13 +4,12 @@ import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
   standalone : true
 })
 export class Contact {
   private dataService = inject(DataService);
-
   profile = this.dataService.get('profile');
 }
